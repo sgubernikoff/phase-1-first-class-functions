b0259220c32a97d11eea316da36d8398a01715cb
Sam Gubernikoff <sgubernikoff@gmail.com>
2022-01-31 21:53:24 -0500

    completed assignment

---
+++ index.js
@@ -0,0 +1,86 @@
+// function Monday() {
+//   runFiveMiles();
+//   liftWeights();
+// }
+
+// function Tuesday() {
+//   console.log("Go for a five-mile run");
+//   console.log("Swim 40 laps");
+// }
+
+// function Wednesday() {
+//   console.log("Go for a five-mile run");
+//   console.log("Go for a five-mile run");
+// }
+
+// function Thursday() {
+//   console.log("Go for a five-mile run");
+//   console.log("Pump iron");
+// }
+
+// function Friday() {
+//   console.log("Go for a five-mile run");
+//   console.log("Swim 40 laps");
+// }
+
+// function runFiveMiles() {
+//   console.log("Go for a five-mile run");
+// }
+
+// function liftWeights() {
+//   console.log("Pump iron");
+// }
+
+// function swimFortyLaps() {
+//   console.log("Swim 40 laps");
+// }
+
+// function exerciseRoutine(postRunActivity) {
+//   runFiveMiles();
+//   postRunActivity();
+// }
+
+// function Monday() {
+//   exerciseRoutine(liftWeights);
+// }
+
+// function exerciseRoutine(postRunActivity) {
+//   runFiveMiles();
+//   postRunActivity();
+// }
+
+// exerciseRoutine(() => console.log("Stretch! Work that core!"));
+
+// function morningRoutine(exercise) {
+//   let breakfast;
+
+//   if (exercise === liftWeights) {
+//     breakfast = "protein bar";
+//   } else if (exercise === swimFortyLaps) {
+//     breakfast = "kale smoothie";
+//   } else {
+//     breakfast = "granola";
+//   }
+
+//   exerciseRoutine(exercise);
+
+//   // we could give this function a name if we wanted to, but since
+//   // it's only available _inside_ morningRoutine(), we don't need to
+//   return function () {
+//     console.log(`Nom nom nom, this ${breakfast} is delicious!`);
+//   };
+// }
+
+// const afterExercise = morningRoutine(liftWeights);
+
+// afterExercise();
+function receivesAFunction(spy) {
+  spy();
+}
+function returnsANamedFunction() {
+  return function thing() {};
+}
+
+function returnsAnAnonymousFunction() {
+  return function () {};
+}
